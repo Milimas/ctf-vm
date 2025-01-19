c6f16588285b79ce0a9e6fc7105c67f5e775f658
Milimas <4929474+Milimas@users.noreply.github.com>
2025-01-19 11:17:46 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash 
 
-install_dir="/sgoinfre/$USER/"
-archive_file="kali-linux-2024.2-qemu-amd64.7z"
-kali_qcow="/sgoinfre/$USER/kali-linux-2024.2-qemu-amd64.qcow2"
+set -e
+
+install_dir="/goinfre/$USER/"
+archive_file="kali-linux-2024.4-qemu-amd64.7z"
+kali_qcow="/goinfre/$USER/kali-linux-2024.4-qemu-amd64.qcow2"
 
 if [ ! -f $kali_qcow ]; then
-	if [ ! -f $archive_file ]; then
-		wget https://cdimage.kali.org/kali-2024.2/$archive_file
+	if [ ! -f /tmp/$archive_file ]; then
+		wget https://cdimage.kali.org/current/$archive_file -O /tmp/$archive_file
 	fi
+	archive_file=/tmp/$archive_file
 
 	# Check if py7zr is installed
 	if ! pip show py7zr &> /dev/null; then
@@ -19,26 +22,26 @@ if [ ! -f $kali_qcow ]; then
   	echo "Extracting $archive_file"
   	python3 -m py7zr x $archive_file $install_dir
  	echo "Extraction complete!"
-	rm -f $archive_file
- 	cp run.sh ~/
-  	chmod u+x ~/run.sh
+	#rm -f $archive_file
+ 	cp run.sh ~/ && chmod u+x ~/run.sh || true
 fi
 
 IMAGE_PATH=$kali_qcow
 SSHPORT=2222
 
+#-display gtk,zoom-to-fit=on,grab-on-hover=on,window-close=off \
+export SDL_ALLOW_ALT_TAB_WHILE_GRABBED=0;
+
 qemu-system-x86_64 \
--display gtk,zoom-to-fit=on,grab-on-hover=on,window-close=off \
+-display sdl,gl=on \
 -cpu host \
 -full-screen \
 -enable-kvm \
--netdev user,id=hostnet0,hostfwd=tcp::${SSHPORT}-:22 \
--usb -device qemu-xhci,id=xhci -device usb-mouse -device usb-kbd \
--device virtio-vga \
--device intel-hda \
--device hda-duplex \
--device virtio-net,netdev=hostnet0 \
+-machine q35,accel=kvm \
+-nic user,model=virtio-net-pci,hostfwd=tcp::2222-:22 \
+-device virtio-vga-gl,xres=1920,yres=1080 \
+-device intel-hda -device hda-duplex -device virtio-serial-pci  \
 -m 12G \
 -smp 20 \
--daemonize \
+-usb -full-screen -daemonize \
 ${IMAGE_PATH}
